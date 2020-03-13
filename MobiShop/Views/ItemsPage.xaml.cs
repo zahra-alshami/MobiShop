5b442560543247e0742964dfd2a54c6b512a1741
Zahra Al-shami <zahrash.95@outlook.com>
2020-03-13 06:39:51 +0200

    Add project files.

---
+++ MobiShop/Views/ItemsPage.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using MobiShop.Models;
+using MobiShop.Views;
+using MobiShop.ViewModels;
+
+namespace MobiShop.Views
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class ItemsPage : ContentPage
+    {
+        ItemsViewModel viewModel;
+
+        public ItemsPage()
+        {
+            InitializeComponent();
+            Device.SetFlags(new[] {
+                "CarouselView_Experimental",
+                "IndicatorView_Experimental"
+            });
+
+            BindingContext = viewModel = new ItemsViewModel();
+      Device.StartTimer(TimeSpan.FromSeconds(5), (Func<bool>)(() =>
+      {
+          TheCarousel.Position = (TheCarousel.Position + 1) % 2;
+          return true;
+      }));  
+        }
+
+        async void OnItemSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            var item = args.SelectedItem as Item;
+            if (item == null)
+                return;
+
+            await Navigation.PushAsync(new ItemDetailPage(new ItemDetailViewModel(item)));
+
+            // Manually deselect item.
+            ItemsListView.SelectedItem = null;
+        }
+
+        async void AddItem_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushModalAsync(new NavigationPage(new NewItemPage()));
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            if (viewModel.Items.Count == 0)
+                viewModel.LoadItemsCommand.Execute(null);
+        }
+    }
+}
